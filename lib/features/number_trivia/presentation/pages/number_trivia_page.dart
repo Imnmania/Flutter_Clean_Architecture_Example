ba14df9cc23e0260b7603de3d9c305f756ea998e
Imnmania <niloyb@live.com>
2023-01-28 21:45:35 +0600

    pixel overflow fix of textfield

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -16,41 +16,45 @@ class NumberTriviaPage extends StatelessWidget {
         surfaceTintColor: Colors.transparent,
         scrolledUnderElevation: 0,
       ),
-      body: BlocProvider(
-        create: (context) => sl<NumberTriviaBloc>(),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 10),
-              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
-                builder: (context, state) {
-                  if (state is EmptyState) {
-                    return const MessageDisplayWidget(
-                      message: '🔍 Start Searching!',
+      body: SingleChildScrollView(
+        child: BlocProvider(
+          create: (context) => sl<NumberTriviaBloc>(),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 10),
+                BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                  builder: (context, state) {
+                    if (state is EmptyState) {
+                      return const MessageDisplayWidget(
+                        message: '🔍 Start Searching!',
+                      );
+                    } else if (state is ErrorState) {
+                      return MessageDisplayWidget(
+                        message: '⚠️ ${state.errorMessage}',
+                      );
+                    } else if (state is LoadingState) {
+                      return const LoadingWidget();
+                    } else if (state is LoadedState) {
+                      return TriviaDisplayWidget(
+                        numberTrivia: state.trivia,
+                      );
+                    }
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: const MessageDisplayWidget(
+                        message: '🔍 Start Searching!',
+                      ),
                     );
-                  } else if (state is ErrorState) {
-                    return MessageDisplayWidget(
-                      message: '⚠️ ${state.errorMessage}',
-                    );
-                  } else if (state is LoadingState) {
-                    return const LoadingWidget();
-                  } else if (state is LoadedState) {
-                    return TriviaDisplayWidget(
-                      numberTrivia: state.trivia,
-                    );
-                  }
-                  return SizedBox(
-                    height: MediaQuery.of(context).size.height / 3,
-                    child: const Placeholder(),
-                  );
-                },
-              ),
-              const SizedBox(height: 10),
-              const TriviaControlWidget(),
-              const SizedBox(height: 10),
-            ],
+                  },
+                ),
+                const SizedBox(height: 10),
+                const TriviaControlWidget(),
+                const SizedBox(height: 10),
+              ],
+            ),
           ),
         ),
       ),
